c2e204280e5e93fed7986f9985a5c1b0623c8f5e
Nikita Isupov <145199579+nifreebie@users.noreply.github.com>
2025-01-23 05:56:09 +0300

    Initial commit

---
+++ src/main/java/nifreebie/fractal_flame_generator_backend/FractalFlameGeneratorBackendApplication.java
@@ -0,0 +1,12 @@
+package nifreebie.fractal_flame_generator_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FractalFlameGeneratorBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FractalFlameGeneratorBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nifreebie/fractal_flame_generator_backend/FractalFlameGeneratorBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package nifreebie.fractal_flame_generator_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FractalFlameGeneratorBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

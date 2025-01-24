8b310ef187e7c8a946a435be996cc0d2f2f11185
Nikita Isupov <145199579+nifreebie@users.noreply.github.com>
2025-01-24 19:09:21 +0300

    feat: add user repository

---
+++ src/main/java/nifreebie/fractal_flame_generator_backend/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package nifreebie.fractal_flame_generator_backend.repository;
+
+import nifreebie.fractal_flame_generator_backend.domain.User;
+import org.springframework.data.repository.CrudRepository;
+import java.util.UUID;
+
+public interface UserRepository extends CrudRepository<User, UUID> {
+    User findByUsername(String username);
+    boolean existsByUsername(String username);
+}

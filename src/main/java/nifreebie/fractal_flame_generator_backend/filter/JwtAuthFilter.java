c8693564160383416f72a9327ca567ae5b6672e1
Nikita Isupov <145199579+nifreebie@users.noreply.github.com>
2025-01-24 19:11:17 +0300

    feat: add util for generation and verification jwt

---
+++ src/main/java/nifreebie/fractal_flame_generator_backend/filter/JwtAuthFilter.java
@@ -0,0 +1,52 @@
+package nifreebie.fractal_flame_generator_backend.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.userdetails.User;
+import nifreebie.fractal_flame_generator_backend.util.JwtUtil;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.UUID;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final RequestMatcher ignoreAuthenticationUrls = request ->
+            request.getRequestURI().startsWith("/api/auth");
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return ignoreAuthenticationUrls.matches(request);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = extractTokenFromHeader(request);
+
+        if (token != null && JwtUtil.validateToken(token)) {
+            UUID userId = JwtUtil.getIdFromToken(token);
+            User user = new User(userId.toString(), "", Collections.emptyList());
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String extractTokenFromHeader(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            return header.substring(7);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/nifreebie/fractal_flame_generator_backend/util/JwtUtil.java
@@ -0,0 +1,45 @@
+package nifreebie.fractal_flame_generator_backend.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.UUID;
+
+public class JwtUtil {
+    private static final String SECRET_KEY = "please_change_this_secret_keyyyyyyyyyyyyyyyyyyyyyy";
+    private static final long EXPIRATION_TIME = 86400000;
+
+    private static final Key key = Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+
+    public static String generateToken(UUID id) {
+        return Jwts.builder()
+                .setSubject(id.toString())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public static boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (JwtException e) {
+            return false;
+        }
+    }
+
+    public static UUID getIdFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        return UUID.fromString(claims.getSubject());
+    }
+}
